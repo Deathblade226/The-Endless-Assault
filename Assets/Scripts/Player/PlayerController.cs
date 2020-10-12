faca5c26135637c62dd97b6247e6060f4800730f
Jason Clarke <jclarke@student.neumont.edu>
2020-10-12 14:23:39 -0600

    basic input system addition to player controller

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.XR.Haptics;
+using static UnityEngine.InputSystem.InputAction;
+
+public class PlayerController : MonoBehaviour {
+    
+[SerializeField]float walkSpeed = 1;
+[SerializeField]float roationSpeed = 1;
+
+private float currentSpeed = 1;
+
+private void Start() {
+	currentSpeed = walkSpeed;		
+}
+
+public void Walk(CallbackContext context) { 
+	Debug.Log("Walk hit");
+}
+public void Jump(CallbackContext context) { 
+	Debug.Log("Jump hit");
+}
+public void Sprint(CallbackContext context) {
+	Debug.Log("Sprint hit");
+}
+
+}
