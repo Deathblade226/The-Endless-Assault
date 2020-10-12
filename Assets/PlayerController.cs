67b4d058eb7daa522961091476f7207a140aed19
Jason Clarke <jclarke@student.neumont.edu>
2020-10-12 14:09:18 -0600

    added unity input system

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    
+[SerializeField]float walkSpeed = 1;
+[SerializeField]float roationSpeed = 1;
+
+void Start() {
+        
+}
+
+void Update() {
+        
+}
+
+}
