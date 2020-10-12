d5c0a40ccb816a6a51c8e51e82d40928b8cc3942
Jason Clarke <jclarke@student.neumont.edu>
2020-10-12 15:15:57 -0600

    input system set up

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,28 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.XR.Haptics;
-using static UnityEngine.InputSystem.InputAction;
 
 public class PlayerController : MonoBehaviour {
     
 [SerializeField]float walkSpeed = 1;
 [SerializeField]float roationSpeed = 1;
 
+private KeyboardMouse controls;
+
 private float currentSpeed = 1;
+
+private void Awake() {
+	controls = new KeyboardMouse();	
+
+	controls.Player.Walk.performed += ctx => Walk();
+}
 
 private void Start() {
 	currentSpeed = walkSpeed;		
 }
 
-public void Walk(CallbackContext context) { 
-	Debug.Log("Walk hit");
+private void Walk() { 
+
 }
-public void Jump(CallbackContext context) { 
-	Debug.Log("Jump hit");
+private void Jump() { 
+
 }
-public void Sprint(CallbackContext context) {
-	Debug.Log("Sprint hit");
+private void Sprint() 
+
+private void OnEnable() {
+	controls.Player.Enable();		
+}
+private void OnDisable() {
+	controls.Player.Disable();		
 }
 
 }
