bc64cf9146b329f773132c74d0ab882098b2cae0
Jason Clarke <jclarke@student.neumont.edu>
2020-10-15 00:08:37 -0600

    setup of smooth thrid person camera

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -9,13 +9,13 @@ public class PlayerController : MonoBehaviour {
     
 [SerializeField]float walkSpeed = 1;
 [SerializeField]float roationSpeed = 1;
-[SerializeField]GameObject camTarget = null;
-[SerializeField]float camClamp = 10;
+//[SerializeField]GameObject camTarget = null;
+//[SerializeField]float camClamp = 10;
 
 private Rigidbody rb;
 private float currentSpeed;
 private Vector2 walkInput;
-private Vector2 rotationInput;
+//private Vector2 rotationInput;
 private float jumpInput;
 private float sprintInput;
 
@@ -30,13 +30,13 @@ private void Start() {
 
 private void FixedUpdate() {
 	transform.Translate(new Vector3((walkInput.x * Time.deltaTime) * currentSpeed,0, (walkInput.y * Time.deltaTime) * currentSpeed));
-	Vector2 mousePosition = rotationInput - new Vector2(Screen.width / 2, Screen.height / 2);
+	//Vector2 mousePosition = rotationInput - new Vector2(Screen.width / 2, Screen.height / 2);
 	//Debug.Log(mousePosition);
-	if(Cursor.lockState != CursorLockMode.Locked) transform.Rotate(Vector3.up, mousePosition.normalized.x);
-	Quaternion rotation = camTarget.transform.rotation;
-	rotation.x += mousePosition.y;
-	Debug.Log(rotation.x);
-	if (rotation.x > -camClamp && rotation.x < camClamp) camTarget.transform.Rotate(Vector3.right, mousePosition.normalized.y);
+	//if(Cursor.lockState != CursorLockMode.Locked) transform.Rotate(Vector3.up, mousePosition.normalized.x);
+	//Quaternion rotation = camTarget.transform.rotation;
+	//rotation.x += mousePosition.y;
+	//Debug.Log(rotation.x);
+	//if (rotation.x > -camClamp && rotation.x < camClamp) camTarget.transform.Rotate(Vector3.right, mousePosition.normalized.y);
 }
 
 public void OnMove(InputAction.CallbackContext context) { 
@@ -48,9 +48,9 @@ public void OnJump(InputAction.CallbackContext context) {
 public void OnSprint(InputAction.CallbackContext context) { 
 	sprintInput = context.ReadValue<float>();
 }
-public void OnRotate(InputAction.CallbackContext context) { 
-	rotationInput = context.ReadValue<Vector2>();
-}
+//public void OnRotate(InputAction.CallbackContext context) { 
+//	rotationInput = context.ReadValue<Vector2>();
+//}
 public void OnToggleCursor(InputAction.CallbackContext context) { 
 	Cursor.lockState = ( Cursor.lockState == CursorLockMode.Locked) ? CursorLockMode.Confined : CursorLockMode.Locked;
 }
