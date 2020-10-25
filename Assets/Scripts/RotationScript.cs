43af9f5a750daff1cedbfd45292d2f71db82bd37
Jason Clarke <jclarke@student.neumont.edu>
2020-10-25 12:58:04 -0600

    Tower 1 full design and logic

---
+++ Assets/Scripts/RotationScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationScript : MonoBehaviour {
+
+[SerializeField] float speed = 1;
+private void Update() {
+	transform.Rotate(Vector3.up, speed);		
+}
+
+}
