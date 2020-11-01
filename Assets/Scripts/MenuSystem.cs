d9c866c42a82720647ae64a009de89a01ce14eae
Jason Clarke <jclarke@student.neumont.edu>
2020-11-01 13:56:11 -0700

    work

---
+++ Assets/Scripts/MenuSystem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSystem : MonoBehaviour {
+
+[Header("Menus")]
+[SerializeField] GameObject titleScreen = null;
+[SerializeField] GameObject lobbyMenu = null;
+
+public void MenueChanger(string menues) { }
+
+}
