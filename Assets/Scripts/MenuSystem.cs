fc4cc9574c6ef9cdaa24ea11fdf970b8e4fbb150
Jason Clarke <jclarke@student.neumont.edu>
2020-11-02 08:04:52 -0700

    Linked the menues

--- Assets/Scripts/MenuSystem.cs
+++ Assets/Scripts/MenuSystem.cs
@@ -1,13 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuSystem : MonoBehaviour {
 
 [Header("Menus")]
 [SerializeField] GameObject titleScreen = null;
 [SerializeField] GameObject lobbyMenu = null;
+[SerializeField] GameObject optionsMenu = null;
+[SerializeField] GameObject helpMenu = null;
 
-public void MenueChanger(string menues) { }
+[Header("Settings")]
+[SerializeField] Slider music = null;
+[SerializeField] Slider sfx = null;
+
+public void Start() {
+	if (PlayerPrefs.HasKey("MusicLevels")) { PlayerPrefs.SetFloat("MusicLevels", 100); }
+	if (PlayerPrefs.HasKey("SFXLevels")) { PlayerPrefs.SetFloat("SFXLevels", 100); }
+}
+
+public void ShowTitle() {
+	titleScreen.SetActive(true);
+	lobbyMenu.SetActive(false);
+	optionsMenu.SetActive(false);
+}
+public void ShowLobby() { 
+	titleScreen.SetActive(false);
+	lobbyMenu.SetActive(true);
+	optionsMenu.SetActive(false);
+}
+public void ShowOptions() { 
+	titleScreen.SetActive(false);
+	lobbyMenu.SetActive(false);
+	optionsMenu.SetActive(true);
+}
+public void ToggleHelp() { 
+	helpMenu.SetActive(!helpMenu.activeSelf);
+}
 
 }
