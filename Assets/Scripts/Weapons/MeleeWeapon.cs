c98910f826d015c5b8b44770cbbce842bcc2eb79
Jason Clarke <jclarke@student.neumont.edu>
2020-11-03 08:07:33 -0700

    stopping damage form being done more then once an attack

--- Assets/Scripts/Weapons/MeleeWeapon.cs
+++ Assets/Scripts/Weapons/MeleeWeapon.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class MeleeWeapon : Weapon {
 
 private void OnTriggerEnter(Collider other) {
-	if (Enemies.Contains(other.tag) && CanAttack) { 
+	if (Enemies.Contains(other.tag) && CanAttack && PhotonNetwork.IsMasterClient) { 
 	CanAttack = false;
 	other.GetComponent<PhotonView>().RPC("ApplyDamage", RpcTarget.All, Damage);
 	}	
