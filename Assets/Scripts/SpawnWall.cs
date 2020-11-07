8edb3756d85ec28f281f5be91734d9dd4e94b5c6
Jason Clarke <jclarke@student.neumont.edu>
2020-11-07 16:51:18 -0700

    Run 2 on second map

---
+++ Assets/Scripts/SpawnWall.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnWall : MonoBehaviour {
+
+[SerializeField] List<string> tags;
+
+private void OnCollisionEnter(Collision collision) {
+	if (tags.Contains(collision.gameObject.tag)) { collision.gameObject.transform.Translate(collision.gameObject.transform.forward/5); }
+}
+
+}
