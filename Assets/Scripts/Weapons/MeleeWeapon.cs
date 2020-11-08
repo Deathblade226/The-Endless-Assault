3fd9eed1da3aa1e9f29902ebee79fed7af0b7124
Jason Clarke <jclarke@student.neumont.edu>
2020-11-07 22:07:24 -0700

    Monsters travel to the objective on the second map and damage it when they attack it

--- Assets/Scripts/Weapons/MeleeWeapon.cs
+++ Assets/Scripts/Weapons/MeleeWeapon.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class MeleeWeapon : Weapon {
 
 private void OnTriggerEnter(Collider other) {
+	if (other.GetComponent<PhotonView>() == null) return;
 	if (Enemies.Contains(other.tag) && (CanAttack || ContinuousAttack) && PhotonNetwork.IsMasterClient) { 
 	if (!ContinuousAttack) CanAttack = false;
 	other.GetComponent<PhotonView>().RPC("ApplyDamage", RpcTarget.All, Damage);
