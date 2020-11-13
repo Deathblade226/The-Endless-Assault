9c59fc616796db8daa78ade6fb1cffdfc83842f1
Jason Clarke <jclarke@student.neumont.edu>
2020-11-13 14:42:02 -0700

    cleaned up shamans particles to match their cast of the healing magic.

--- Assets/Scripts/Weapons/HealWeapon.cs
+++ Assets/Scripts/Weapons/HealWeapon.cs
@@ -10,17 +10,19 @@ public class HealWeapon : Weapon {
 [SerializeField] float healRange = 0;
 [SerializeField] LayerMask layers;
 [SerializeField] Transform healPoint;
+[SerializeField] GameObject particle;
 
 private bool healed = true;
 private float healCD = 0;
 
 void Update() {
+    if (!particle.GetComponent<ParticleSystem>().isEmitting) particle.SetActive(false);
     if (!healed && healCD <= 0) { 
+    particle.GetComponent<ParticleSystem>().Stop();
     healed = true;
     Collider[] colliders = Physics.OverlapSphere(healPoint.position, healRange, layers);
-    foreach(Collider collider in colliders) { 
-    collider.gameObject.GetComponent<PhotonView>().RPC("ApplyDamage", RpcTarget.All, -healAmount);
-    }
+    particle.SetActive(true);
+    if (colliders.Length > 0) { foreach (Collider collider in colliders) { collider.gameObject.GetComponent<PhotonView>().RPC("ApplyDamage", RpcTarget.All, -healAmount); }  }
     } else if (healCD > 0) { healCD -= Time.deltaTime; }
 }
 
