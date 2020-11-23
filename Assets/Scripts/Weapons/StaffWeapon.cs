b92044c43e6cb82d1d7c00af125ea2a527efa57b
Jason Clarke <jclarke@student.neumont.edu>
2020-11-23 06:42:26 -0700

    fixed summoners alt attack

--- Assets/Scripts/Weapons/StaffWeapon.cs
+++ Assets/Scripts/Weapons/StaffWeapon.cs
@@ -7,8 +7,12 @@ public class StaffWeapon : Weapon {
 
 [SerializeField] GameObject projectile;
 [SerializeField] Transform spawnpoint;
+[SerializeField] bool lookAtTarget = false;
+[SerializeField] string enemy = "";
 [SerializeField] float waitTime = 1;
 [SerializeField] float shotSpeed = 1;
+
+[SerializeField] VisionSystem vs = null;
 
 private float waitCD = 0;
 private bool cast = false;
@@ -28,7 +32,10 @@ private void Spawn(int id) {
 	if (projectile != null) { 
 	GameObject shot = PhotonView.Find(id).gameObject;
 	transform.rotation = spawnpoint.rotation;
-	shot.GetComponent<Projectile>().WeaponDamage = Damage;
+	if (lookAtTarget && vs != null)  { shot.transform.LookAt(vs.SeenTarget.transform.position); }
+	Projectile projectile = shot.GetComponent<Projectile>();
+	projectile.WeaponDamage = Damage;
+	projectile.EnemyTag = (vs != null) ? vs.SeenTarget.tag : enemy;
 	Rigidbody shotRB = shot.GetComponent<Rigidbody>();
 	shotRB.useGravity = false;
 	shotRB.AddForce(spawnpoint.transform.forward * shotSpeed * 10, ForceMode.Acceleration);
